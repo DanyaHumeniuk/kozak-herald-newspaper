0af5167ccb1f97d03241d29cf10097f31fe987c0
DanyaHumeniuk <dgumenuk076@gmail.com>
2025-05-17 15:14:11 -0700

    Added a Footer section

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+
+
+const Footer = () => {
+  return (
+    <div className="flex w-full min-h-[200px] bg-customBlue">
+        Hello
+    </div>
+  )
+}
+
+export default Footer
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,19 +7,19 @@ const Hero = () => {
         <div className="bg-gray-600 mt-14 w-10/12 min-h-12 rounded-lg text-transparent">Hello</div>
 
         <div className="mt-14 w-10/12 min-h-80 sm:min-h-60 flex flex-row gap-8">
-            <div className="bg-gray-600 w-3/4 rounded-md text-transparent">Sup</div>
-            <div className="bg-gray-600 w-1/4 rounded-md text-transparent">Hey</div>
+            <div className="bg-gray-600 w-1/2 lg:w-3/4 rounded-md text-transparent">Sup</div>
+            <div className="bg-gray-600 w-1/2 lg:w-1/4 rounded-md text-transparent">Hey</div>
         </div>
 
         <div className="bg-gray-600 mt-14 w-10/12 min-h-12 rounded-lg text-transparent">Hello</div>
 
        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 my-14 w-10/12 mx-auto">
-          <div className="bg-gray-600 rounded shadow p-4 text-transparent">Helo</div>
-          <div className="bg-gray-600 rounded shadow p-4 text-transparent">Helo</div>
-          <div className="bg-gray-600 rounded shadow p-4 text-transparent">Helo</div>
-          <div className="bg-gray-600 rounded shadow p-4 text-transparent">Helo</div>
-          <div className="bg-gray-600 rounded shadow p-4 text-transparent">Helo</div>
-          <div className="bg-gray-600 rounded shadow p-4 text-transparent">Helo</div>
+          <div className="bg-gray-600 rounded-md shadow p-4 text-transparent min-h-60">Helo</div>
+          <div className="bg-gray-600 rounded-md shadow p-4 text-transparent min-h-60">Helo</div>
+          <div className="bg-gray-600 rounded-md shadow p-4 text-transparent min-h-60">Helo</div>
+          <div className="bg-gray-600 rounded-md shadow p-4 text-transparent min-h-60">Helo</div>
+          <div className="bg-gray-600 rounded-md shadow p-4 text-transparent min-h-60">Helo</div>
+          <div className="bg-gray-600 rounded-md shadow p-4 text-transparent min-h-60">Helo</div>
         </div>
     </div>
   )

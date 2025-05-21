c71883112f3981de5ee887d1701c2dfbc485ff65
DanyaHumeniuk <dgumenuk076@gmail.com>
2025-05-20 17:21:53 -0700

    Edited the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,21 @@
-
+import kozak from "../assets/kozakLogo.png"
 
 const Footer = () => {
   return (
-    <div className="flex w-full min-h-[200px] bg-customBlue">
-        Hello
+    <div className="flex flex-col w-full min-h-[200px] bg-customBlue items-center justify-center">
+
+      <div className="w-10/12 m-10 flex flex-row items-center">
+        <div>
+          <img src={kozak} alt="Kozak Logo" className="h-auto w-auto max-h-[150px] max-w-[150px] lg:max-h-[180px] lg:max-w-[180px] rounded-md"/>
+          <p className="text-sm max-w-[150px] text-white">Kozak Herald: Ukrainian Spirit, Island Home</p>
+        </div>
+
+        
+        
+      </div>
+
+      <div className="text-transparent bg-blue-950 w-full">End</div>
+      
     </div>
   )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,23 +4,23 @@ const Hero = () => {
   return (
     <div className="flex flex-col items-center w-full h-full ">
 
-        <div className="bg-gray-600 mt-14 w-10/12 min-h-12 rounded-lg text-transparent">Hello</div>
+      <div className="bg-gray-600 mt-14 w-10/12 min-h-12 rounded-lg text-transparent">Hello</div>
 
-        <div className="mt-14 w-10/12 min-h-80 sm:min-h-60 flex flex-row gap-8">
-            <div className="bg-gray-600 w-1/2 lg:w-3/4 rounded-md text-transparent">Sup</div>
-            <div className="bg-gray-600 w-1/2 lg:w-1/4 rounded-md text-transparent">Hey</div>
-        </div>
+      <div className="mt-14 w-10/12 min-h-80 sm:min-h-60 flex flex-row gap-8">
+          <div className="bg-gray-600 w-1/2 lg:w-3/4 rounded-md text-transparent">Sup</div>
+          <div className="bg-gray-600 w-1/2 lg:w-1/4 rounded-md text-transparent">Hey</div>
+      </div>
 
-        <div className="bg-gray-600 mt-14 w-10/12 min-h-12 rounded-lg text-transparent">Hello</div>
+      <div className="bg-gray-600 mt-14 w-10/12 min-h-12 rounded-lg text-transparent">Hello</div>
 
-       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 my-14 w-10/12 mx-auto">
-          <div className="bg-gray-600 rounded-md shadow p-4 text-transparent min-h-60">Helo</div>
-          <div className="bg-gray-600 rounded-md shadow p-4 text-transparent min-h-60">Helo</div>
-          <div className="bg-gray-600 rounded-md shadow p-4 text-transparent min-h-60">Helo</div>
-          <div className="bg-gray-600 rounded-md shadow p-4 text-transparent min-h-60">Helo</div>
-          <div className="bg-gray-600 rounded-md shadow p-4 text-transparent min-h-60">Helo</div>
-          <div className="bg-gray-600 rounded-md shadow p-4 text-transparent min-h-60">Helo</div>
-        </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 my-14 w-10/12 mx-auto">
+        <div className="bg-gray-600 rounded-md shadow p-4 text-transparent min-h-60">Helo</div>
+        <div className="bg-gray-600 rounded-md shadow p-4 text-transparent min-h-60">Helo</div>
+        <div className="bg-gray-600 rounded-md shadow p-4 text-transparent min-h-60">Helo</div>
+        <div className="bg-gray-600 rounded-md shadow p-4 text-transparent min-h-60">Helo</div>
+        <div className="bg-gray-600 rounded-md shadow p-4 text-transparent min-h-60">Helo</div>
+        <div className="bg-gray-600 rounded-md shadow p-4 text-transparent min-h-60">Helo</div>
+      </div>
     </div>
   )
 }
